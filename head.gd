06dc7107a9aa2bfa7d2627b46190978034f73a4f
Someoneyeeting <57325386+Someoneyeeting@users.noreply.github.com>
2024-08-17 04:15:25 +0300

    extend

--- head.gd
+++ head.gd
@@ -10,16 +10,20 @@ const BODY = preload("res://deadbody.tscn")
 var cols := []
 var poses = []
 var off := Vector2.ZERO
+var lastdir = Vector2.ZERO
 var frameedit = false
+var plannedmoves = []
 
 func add_tail(tail = null):
 	if(not tail):
 		tail = TAIL.instantiate()
-		add_child(tail)
+		add_child.call_deferred(tail)
 	tail.ind = cols.size()
+	tail.position = cols[-1].position if cols.size() > 0 else Vector2.ZERO
 	cols.append(tail)
 	tail.modulate = lerp(Color.WHITE,Color.BLACK,float(cols.size()) / length)
 	tail.cut.connect(cut)
+	tail.extend.connect(extend)
 	return tail
 	
 func _ready() -> void:
@@ -29,6 +33,14 @@ func _ready() -> void:
 	
 	add_tail($tail)
 	$rayspos.reparent(cols[0])
+
+func extend():
+	var t = add_tail()
+	t.position -= poses[-1]
+	plannedmoves.append(lastdir)
+	#move(lastdir)
+	length += 1
+	
 
 func cut(ind):
 	if(ind == 0 or ind >= cols.size()):
@@ -41,6 +53,17 @@ func cut(ind):
 			cols[i].reparent(body)
 			cols[i].cut.disconnect(cut)
 		get_parent().add_child.call_deferred(body)
+
+func move(dir):
+	off += dir
+	poses.insert(0,dir)
+	#$CollisionShape2D.position = off
+	while(poses.size() > cols.size()):
+		poses.pop_back()
+	
+	for i in min(cols.size(),poses.size()):
+		cols[i].position += poses[i]
+	lastdir = dir
 
 func _input(event: InputEvent) -> void:
 	var dir = Vector2.ZERO
@@ -60,14 +83,7 @@ func _input(event: InputEvent) -> void:
 		return
 	
 	frameedit = true
-	off += dir
-	poses.insert(0,dir)
-	#$CollisionShape2D.position = off
-	while(poses.size() > cols.size()):
-		poses.pop_back()
-	
-	for i in range(poses.size()):
-		cols[i].position += poses[i]
+	plannedmoves.append(dir)
 	
 
 
@@ -80,6 +96,17 @@ func _physics_process(delta: float) -> void:
 		cols.pop_back()
 	while(cols.size() < length):
 		var tail = add_tail()
-		tail.position = cols[-1].position
+		#move(lastdir)
+	
+	for i in cols.size():
+		cols[i].ishead = false
+		cols[i].isend = false
+		cols[i].ind = i
+	length = cols.size()
+	cols[0].ishead = true
+	cols[-1].isend = true
 	frameedit = false
+	for i in plannedmoves:
+		move(i)
+	plannedmoves = []
 	#print($CharacterBody2D.is_on_floor())
--- tail.gd
+++ tail.gd
@@ -3,13 +3,19 @@ extends CollisionShape2D
 
 
 signal cut(ind)
+signal extend
 
 var ind = -1
 
+var ishead = false
+var isend = false
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if(area.is_in_group("cut")):
 		cut.emit(ind)
+	if(area.is_in_group("longer") and ishead):
+		extend.emit()
+		area.queue_free()
 
 func _on_area_2d_2_area_entered(area: Area2D) -> void:
 	pass # R
